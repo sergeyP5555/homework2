d83b0b79a0ab62adb159ba19ba9dd67de1ed8781
SergeyP5555 <popserg5555@gmail.com>
2021-10-12 14:59:38 +0300

    homework 2

---
+++ homework-php.php
@@ -0,0 +1,15 @@
+<?php
+$name = "Sergey";
+echo $name . "\n";
+$age = 25;
+echo $age . "\n";
+$pi = 3.14;
+echo $pi . "\n";
+$array1 = ['alex', 'vova', 'tolya'];
+print_r($array1);
+$array2 = ['alex', 'vova', 'tolya', ['kostya', 'olya']];
+print_r($array2);
+$array3 = ['alex', 'vova', 'tolya', ['kostya', 'olya', ['gosha', 'mila']]];
+print_r($array3);
+$array4 = [['alex', 'vova', 'tolya'], ['kostya', 'olya'], ['gosha', 'mila']];
+print_r($array4);
